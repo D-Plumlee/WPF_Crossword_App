fb5d360720e1b45ae5caaa70b9bf48950214d356
D-Plumlee <donovan.plumlee@gmail.com>
2026-06-20 11:04:53 -0500

    Reconfigured files to get both projects in a solution.

---
+++ WPF_Crossword_Api_App/Crossword_Backend_Library/ApiAndFileClasses/AiClueFetcher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crossword_Backend_Library.ApiAndFileClasses
+{
+    internal class AiClueFetcher
+    {
+    }
+}
